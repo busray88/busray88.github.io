827b7032d6a00d0d3a3c7cf6caad61856b66c44e
busray88 <106096951+busray88@users.noreply.github.com>
2022-05-23 19:15:45 +0300

    1

---
+++ java/java.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
+
+
+
+function haritayaGit(){
+    var slider =document.getElementById("slider");
+}
+   
+
+
+document.querySelector(".im").src = "1.jpg";
+
+
+
+var niyet=document.getElementById("nyt");
+function renkDegistir(){
+  nyt.style.color="yellow";
+}
+
+
+function bankFunction() {
+  document.getElementById("bank").style.color = "red";
+}
+
+
+
+function iletFunction() {
+  document.getElementById("ilet").style.color = "blue";
+}
+            
+
+function byildirim(){
+  var byildirim=document.getElementById("byildirim");
+  <i class="fas fa-creative-commons-by    "></i>
+byildirim.innerHTML="Siteyi Hazırlayan:Tıkla Ve İsmimi Gör";
+}
+
+
+var tesekkur=document.getElementById("tesekkur");
+function tesekkur(){
+
+  tesekkur.style.color="red";
+}
+
+
+
+function  yazi(){
+tesekkur.style.color="black";
+}
+
+
+function uyeol() {
+  var uyeol = document.getElementById("uyeol");
+  uyeol.style.display = "block";
+  }
+
